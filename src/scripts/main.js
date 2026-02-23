9a6aa95cb0800782e8ff4b1f54650c21f2343fdb
Anton K <drvirus15@gmail.com>
2026-02-23 22:59:31 +0200

    PC

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,59 @@ import { GetBacon } from './utils';
 const baconEl = document.querySelector('.bacon');
 const timerDiv = document.querySelector('.timer');
 
-function timer() {
-  const timerTitle = document.createElement('h2');
-  timerTitle.innerHTML = 'Timer';
-  timerDiv.append(timerTitle);
-}
-
 function runTimer() {
   const ukrainianDate = moment().format('dddd, D MMMM YYYY');
   const newText = `Створено: ${ukrainianDate}`;
   timerDiv.append(newText);
 }
 
+function renderButtons() {
+  const startButton = document.createElement('button');
+  const plusButton = document.createElement('button');
+  const minusButton = document.createElement('button');
+  startButton.className = 'start';
+  plusButton.className = 'plusMin';
+  minusButton.className = 'minusMin';
+  startButton.innerText = 'Start';
+  plusButton.innerText = '+';
+  minusButton.innerText = '-';
+  timerDiv.append(minusButton, plusButton, startButton);
+}
+
+function showMessage() {
+  const timerTitle = document.createElement('h2');
+  timerTitle.innerHTML = 'Timer';
+  const statusMessage = document.createElement('p');
+  statusMessage.innerHTML = 'Вкажіть час в хвилинах';
+  timerDiv.append(timerTitle, statusMessage);
+  return statusMessage;
+}
+
+function timer() {
+  const status = showMessage();
+  renderButtons();
+  const timerCount = document.createElement('p');
+  let timerNumber = 0;
+  timerCount.innerHTML = timerNumber;
+  timerDiv.append(timerCount);
+
+  timerDiv.addEventListener('click', (event) => {
+    if (event.target.className === 'start') {
+      runTimer();
+      status.innerHTML = 'Залишилось';
+    }
+    if (event.target.className === 'plusMin') {
+      timerNumber += 1;
+      timerCount.innerHTML = +timerNumber;
+    }
+    if (event.target.className === 'minusMin') {
+      timerNumber -= 1;
+      timerCount.innerHTML = +timerNumber;
+    }
+  });
+}
+
 timer();
-runTimer();
 
 GetBacon()
   .then((res) => {
