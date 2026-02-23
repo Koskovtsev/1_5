d58adc63e3b90d91014b5258e60bd234e8e3ebca
Lenovo <drvirus15@gmail.com>
2026-02-23 21:45:08 +0200

    timer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,23 @@
+import moment from 'moment';
 import { GetBacon } from './utils';
 
 const baconEl = document.querySelector('.bacon');
+const timerDiv = document.querySelector('.timer');
+
+function timer() {
+  const timerTitle = document.createElement('h2');
+  timerTitle.innerHTML = 'Timer';
+  timerDiv.append(timerTitle);
+}
+
+function runTimer() {
+  const ukrainianDate = moment().format('dddd, D MMMM YYYY');
+  const newText = `Створено: ${ukrainianDate}`;
+  timerDiv.append(newText);
+}
+
+timer();
+runTimer();
 
 GetBacon()
   .then((res) => {
