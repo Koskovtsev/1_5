ff14439660ef9bdf3f8c84ae789f8ca53bafcd83
Lenovo <drvirus15@gmail.com>
2026-02-24 19:12:55 +0200

    timer_is_works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,41 @@
 import moment from 'moment';
-import { GetBacon } from './utils';
 
-const baconEl = document.querySelector('.bacon');
 const timerDiv = document.querySelector('.timer');
+let timerNumber = 0;
 
-function runTimer() {
-  const ukrainianDate = moment().format('dddd, D MMMM YYYY');
+function runTimer(minutes) {
+  let totalSeconds = minutes * 60;
+  const timerCounDown = document.querySelector('.timer-countdown');
+  let setMoment = moment.duration(totalSeconds, 'seconds');
+  let showRemainingTimeInSeconds = moment.utc(0);
+  showRemainingTimeInSeconds.add(setMoment);
+  timerCounDown.innerHTML = showRemainingTimeInSeconds.format('mm:ss');
+  if (totalSeconds > 0) {
+    totalSeconds -= 1;
+  } else {
+    const startButton = document.querySelector('.start');
+    startButton.style.display = 'block';
+    timerNumber = 0;
+  }
+  const myInterval = setInterval(() => {
+    setMoment = moment.duration(totalSeconds, 'seconds');
+    showRemainingTimeInSeconds = moment.utc(0);
+    showRemainingTimeInSeconds.add(setMoment);
+    timerCounDown.innerHTML = showRemainingTimeInSeconds.format('mm:ss');
+    if (totalSeconds > 0) {
+      totalSeconds -= 1;
+    } else {
+      clearInterval(myInterval);
+      const startButton = document.querySelector('.start');
+      startButton.style.display = 'block';
+      const statusMessage = document.querySelector('.status_message');
+      statusMessage.innerHTML = 'Вкажіть час в хвилинах';
+      const timerCount = document.querySelector('.timer-countdown');
+      timerNumber = 0;
+      timerCount.innerHTML = timerNumber;
+    }
+  }, 1000);
+  const ukrainianDate = moment().format('dddd, D MMMM YYYY mm:ss');
   const newText = `Створено: ${ukrainianDate}`;
   timerDiv.append(newText);
 }
@@ -23,47 +53,35 @@ function renderButtons() {
   timerDiv.append(minusButton, plusButton, startButton);
 }
 
-function showMessage() {
-  const timerTitle = document.createElement('h2');
-  timerTitle.innerHTML = 'Timer';
-  const statusMessage = document.createElement('p');
-  statusMessage.innerHTML = 'Вкажіть час в хвилинах';
-  timerDiv.append(timerTitle, statusMessage);
-  return statusMessage;
-}
-
 function timer() {
-  const status = showMessage();
   renderButtons();
+  const statusMessage = document.querySelector('status_message');
+  const startButton = document.querySelector('.start');
   const timerCount = document.createElement('p');
-  let timerNumber = 0;
+  timerCount.className = 'timer-countdown';
   timerCount.innerHTML = timerNumber;
   timerDiv.append(timerCount);
 
   timerDiv.addEventListener('click', (event) => {
-    if (event.target.className === 'start') {
-      runTimer();
-      status.innerHTML = 'Залишилось';
+    const { target } = event;
+    if (target.className === 'start' && timerNumber > 0) {
+      target.style.display = 'none';
+      runTimer(timerNumber);
+      statusMessage.innerHTML = 'Залишилось';
     }
-    if (event.target.className === 'plusMin') {
-      timerNumber += 1;
-      timerCount.innerHTML = +timerNumber;
+    if (target.className === 'plusMin') {
+      if (startButton.style.display !== 'none') {
+        timerNumber += 1;
+        timerCount.innerHTML = +timerNumber;
+      }
     }
-    if (event.target.className === 'minusMin') {
-      timerNumber -= 1;
-      timerCount.innerHTML = +timerNumber;
+    if (target.className === 'minusMin') {
+      if (timerNumber !== 0 && startButton.style.display !== 'none') {
+        timerNumber -= 1;
+        timerCount.innerHTML = +timerNumber;
+      }
     }
   });
 }
 
 timer();
-
-GetBacon()
-  .then((res) => {
-    const markup = res.map((val) => `<p>${val}</p>`).join('');
-    baconEl.innerHTML = markup;
-  })
-  .catch((err) => {
-    baconEl.innerHTML = err;
-    return err;
-  });
